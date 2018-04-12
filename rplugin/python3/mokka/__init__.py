00e4d4a0dcfdbea32fb9c113d1861e9c36fe822d
Lars Franke <frcl@mailbox.org>
2018-04-12 17:27:25 +0200

    always evaluate when command is run

--- rplugin/python3/mokka/__init__.py
+++ rplugin/python3/mokka/__init__.py
@@ -12,25 +12,24 @@ class Mokka(object):
 
     @neovim.function('RunLine')
     def run_line(self, args):
-        if self.nvim.current.line.strip().endswith('?'):
-            lineno, _ = self.nvim.current.window.cursor
-            buf = self.nvim.current.buffer
+        lineno, _ = self.nvim.current.window.cursor
+        buf = self.nvim.current.buffer
 
-            # match current indention level and
-            # append spaces if last line end with ':'
-            spaces = re.match(r'^(\s*)', buf[lineno-2]).group(1)
-            spaces += '    ' if buf[lineno-2].strip().endswith(':') else ''
+        # match current indention level and
+        # append spaces if last line end with ':'
+        spaces = re.match(r'^(\s*)', buf[lineno-2]).group(1)
+        spaces += '    ' if buf[lineno-2].strip().endswith(':') else ''
 
-            interpreter = Interpreter(filename=buf.name)
+        interpreter = Interpreter(filename=buf.name)
 
-            # run with `pass` appended using precalulated spaces
-            source = '\n'.join(buf[0:lineno-1]+[spaces+'pass'])
+        # run with `pass` appended using precalulated spaces
+        source = '\n'.join(buf[0:lineno-1]+[spaces+'pass'])
 
-            if interpreter.exec(source):
-                pass # TODO: show traceback
-            else:
-                value = interpreter.eval(buf[lineno-1])
-                rep = str(value).replace('\n', '')
-                if len(rep) > 32:
-                    rep = rep[:29]+'...'
-                buf[lineno-1] = buf[lineno-1][:-1] + rep
+        if interpreter.exec(source):
+            pass # TODO: show traceback
+        else:
+            value = interpreter.eval(buf[lineno-1])
+            rep = str(value).replace('\n', '')
+            if len(rep) > 32:
+                rep = rep[:29]+'...'
+            buf[lineno-1] = buf[lineno-1] + ' # ' + rep
