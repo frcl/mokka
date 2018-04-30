0d46ef43d754777f9258c6fafea5557fe7c321fe
Lars Franke <frcl@mailbox.org>
2018-04-30 18:34:26 +0200

    remove context free eval function

--- ftplugin/python_mokka.vim
+++ ftplugin/python_mokka.vim
@@ -1,4 +1,3 @@
 " Mokka - Python Code Evaluation Plugin
 
-nnoremap <leader>rl :call EvalLine()<cr>
 nnoremap <leader>ev :call EvalUntilCurrent()<cr>
--- rplugin/python3/mokka/__init__.py
+++ rplugin/python3/mokka/__init__.py
@@ -10,17 +10,6 @@ class Mokka(object):
     def __init__(self, nvim):
         self.nvim = nvim
 
-    @neovim.function('EvalLine')
-    def eval_line(self, args):
-        lineno, _ = self.nvim.current.window.cursor
-        buf = self.nvim.current.buffer
-        interpreter = Interpreter(filename=buf.name)
-        value = interpreter.eval(buf[lineno-1])
-        rep = str(value).replace('\n', '')
-        if len(rep) > 32:
-            rep = rep[:29]+'...'
-        buf[lineno-1] = buf[lineno-1] + ' # ' + rep
-
     @neovim.function('EvalUntilCurrent')
     def eval_until_current(self, args):
         lineno, _ = self.nvim.current.window.cursor
